5cf9cf7196848664452156fd43230a7432850ef2
SochnuyFrykt <serebrenikovdanil@gmail.com>
2025-08-23 03:10:43 +0400

    add routes configuration

--- storage-app/src/main.tsx
+++ storage-app/src/main.tsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./components/pages/home/HomePage.tsx";
+import AddBookPage from "./components/pages/add_book/AddBookPage.tsx";
+import About from "./components/pages/about/About.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <HomePage/>
+      },
+      {
+        path: '/about',
+        element: <About/>
+      },
+      {
+        path: '/addbook',
+        element: <AddBookPage/>
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
