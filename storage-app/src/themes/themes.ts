52e0bf14f7177dc338c0ac08ec4df0319af9246b
SochnuyFrykt <serebrenikovdanil@gmail.com>
2025-08-23 03:18:04 +0400

    add themes

---
+++ storage-app/src/themes/themes.ts
@@ -0,0 +1,20 @@
+export interface ThemeProps {
+  body: string;
+  text: string;
+  toggleBorder: string;
+  background: string;
+}
+
+export const lightTheme: ThemeProps = {
+  body: "",
+  text: "",
+  toggleBorder: "",
+  background: ""
+}
+
+export const darkTheme: ThemeProps = {
+  body: "",
+  text: "",
+  toggleBorder: "",
+  background: ""
+}
